b276173bd85a4aea1fb878afb5228f5c2e90ef44
Denis <kozyrevd2001@ukr.net>
2021-08-07 14:31:36 +0300

    Добавьте файлы проекта.

---
+++ Airline.DAL/Context/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airline.Domain.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Airline.DAL.Context
+{
+    public class ApplicationContext : IdentityDbContext
+    {
+        public ApplicationContext(string connectionstring) : base(connectionstring) { }
+
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+    }
+}
---
+++ Airline.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Airline.Domain.Entities;
+
+namespace Airline.DAL.Interfaces
+{
+    public interface IRepository<T> where T: BaseEntity
+    {
+        Task<IQueryable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task DeleteById(int id);
+        Task Delete(T entity);
+        Task<int> Save();
+
+    }
+}
